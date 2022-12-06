af75718b2f1fbf6f1073dbd593686112d7ccd7d5
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-12-06 13:19:16 +0530

    chore: header updated

--- front-end/src/components/AppLayout/index.tsx
+++ front-end/src/components/AppLayout/index.tsx
@@ -1,4 +1,4 @@
-// Copyright 2019-2020 @Premiurly/polkassembly authors & contributors
+// Copyright 2022-2023 @Abdullah-taqi/PolkaSafe authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 import { Layout } from 'antd';
